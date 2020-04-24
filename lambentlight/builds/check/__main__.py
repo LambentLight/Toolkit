b968ebc5ccb38779afe67316e5661ce07ac3e65b
Hannele Ruiz <justlemoncl@gmail.com>
2020-04-24 09:48:15 -0400

    Changed the exit codes for the build checks

--- lambentlight/builds/check/__main__.py
+++ lambentlight/builds/check/__main__.py
@@ -16,12 +16,12 @@ def main():
     # If the builds do not exist, exit with code 5
     if not os.path.isfile("builds.json"):
         print(f"{Fore.LIGHTRED_EX}The file {Fore.WHITE}builds.json {Fore.LIGHTRED_EX}does not exists!")
-        sys.exit(5)
+        sys.exit(10)
 
     # If the schema does not exists, exit with code 6
     if not os.path.isfile("schema.json"):
         print(f"{Fore.LIGHTRED_EX}The {Fore.WHITE}JSON Schema {Fore.LIGHTRED_EX}does not exists!")
-        sys.exit(6)
+        sys.exit(11)
 
     # If both files exist, load them up as JSON
     with open("builds.json") as file:
@@ -29,13 +29,13 @@ def main():
             builds = json.load(file)
         except json.JSONDecodeError:
             print(f"{Fore.MAGENTA}The file {Fore.WHITE}builds.json {Fore.MAGENTA}contains invalid JSON!")
-            sys.exit(7)
+            sys.exit(12)
     with open("schema.json") as file:
         try:
             schema = json.load(file)
         except json.JSONDecodeError:
             print(f"{Fore.MAGENTA}The {Fore.WHITE}JSON Schema {Fore.MAGENTA}contains invalid JSON!")
-            sys.exit(8)
+            sys.exit(13)
 
     # With the JSON parsed, validate the schema
     try:
@@ -45,7 +45,7 @@ def main():
         print(f"{Fore.CYAN}The {Fore.WHITE}Builds File {Fore.CYAN}does not complies with "
               f"the {Fore.WHITE}JSON Schema{Fore.CYAN}:")
         print(f"\t{Fore.WHITE}{e.message} on {path}")
-        sys.exit(9)
+        sys.exit(14)
 
 
 if __name__ == '__main__':

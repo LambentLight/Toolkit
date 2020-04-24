75c539992132dee66ebc3ab2e5a5194481c7e85f
Hannele Ruiz <justlemoncl@gmail.com>
2020-04-24 10:47:45 -0400

    Allow all files to be checked against the schema

---
+++ lambentlight/check/__main__.py
@@ -0,0 +1,70 @@
+import json
+import os.path
+import sys
+
+import requests
+from colorama import init, Fore
+from jsonschema import validate, ValidationError
+
+
+def main():
+    """
+    Checks that a JSON file matches the Schema.
+    """
+    # Initialize the console colors
+    init()
+
+    # If no parameters were specified, exit
+    if len(sys.argv) < 2:
+        print(f"{Fore.LIGHTRED_EX}You need to specify the file to check!")
+        sys.exit(10)
+
+    # Save the name of the file
+    name = sys.argv[1]
+
+    # If the schema does not exists, exit with code 6
+    if not os.path.isfile(name):
+        print(f"{Fore.LIGHTRED_EX}The file {Fore.WHITE}{name} {Fore.LIGHTRED_EX}does not exists!")
+        sys.exit(11)
+
+    # If the file exist, load it as JSON
+    with open(name) as file:
+        try:
+            origin = json.load(file)
+        except json.JSONDecodeError:
+            print(f"{Fore.LIGHTRED_EX}The file {Fore.WHITE}{name} {Fore.LIGHTRED_EX}contains invalid JSON!")
+            sys.exit(12)
+
+    # If the file does not has a schema, exit
+    if "$schema" not in origin:
+        print(f"{Fore.LIGHTRED_EX}The file {Fore.WHITE}{name} {Fore.LIGHTRED_EX}doesnt have a schema!")
+        sys.exit(13)
+
+    # Request the schema
+    req = requests.get(origin["$schema"])
+    # Exit if we got an error code that is not 200
+    if req.status_code != 200:
+        print(f"{Fore.LIGHTRED_EX}Got code {Fore.WHITE}{req.status_code} {Fore.LIGHTRED_EX}while downloading Schema!")
+        sys.exit(14)
+    # Then, try to parse and exit if we fail
+    try:
+        schema = json.loads(req.text)
+    except json.JSONDecodeError:
+        print(f"{Fore.LIGHTRED_EX}The {Fore.WHITE}JSON Schema {Fore.LIGHTRED_EX}is not valid JSON!")
+        sys.exit(15)
+
+    # With the JSON parsed, validate the schema
+    try:
+        validate(origin, schema)
+    except ValidationError as e:
+        path = " > ".join(str(x) for x in e.path)
+        print(f"{Fore.CYAN}The File {Fore.WHITE}{name} {Fore.CYAN}does not complies with "
+              f"the {Fore.WHITE}JSON Schema{Fore.CYAN}:")
+        print(f"\t{Fore.WHITE}{e.message} on {path}")
+        sys.exit(16)
+    else:
+        print(f"{Fore.LIGHTGREEN_EX}The File {Fore.WHITE}{name} {Fore.LIGHTGREEN_EX}complies with the Schema!")
+
+
+if __name__ == '__main__':
+    main()
